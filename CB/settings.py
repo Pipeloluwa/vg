01832cc15a5b6b4bd3748f4e679756a8f6a44c9a
Pipeloluwa <91763066+Pipeloluwa@users.noreply.github.com>
2022-03-07 00:17:48 +0100

    changed

--- CB/settings.py
+++ CB/settings.py
@@ -123,9 +123,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL='/files/'
 if DEBUG:
-    STATIC_DIRS = [os.path.join(BASE_DIR), "static"]
+    STATIC_DIRS = [os.path.join(BASE_DIR), "staticfiles"]
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
